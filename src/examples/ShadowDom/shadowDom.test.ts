e3f50b461b19bff24582e6da13b6f83d85f20e61
Zach Lankton <zachlankton@gmail.com>
2023-08-24 20:57:53 -0400

    update ShadowDom test name

--- src/examples/ShadowDom/shadowDom.test.ts
+++ src/examples/ShadowDom/shadowDom.test.ts
@@ -3,8 +3,8 @@ import { screen } from "@testing-library/dom";
 import { render } from "rezact";
 import { Page } from "./ShadowDom";
 
-describe("Hello World suite", () => {
-  it("Single Hello World", () => {
+describe("Shadow DOM suite", () => {
+  it("Renders an Element with scoped CSS using shadow DOM", () => {
     // render(document.body, Page);
     // const allHeaders = screen.getAllByText(/hello world/i);
     // expect(allHeaders).toHaveLength(1);
