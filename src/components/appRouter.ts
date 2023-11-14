49c57592515fd50c8238fa10ee266e8906517c1b
Zach Lankton <zachlankton@gmail.com>
2023-11-13 22:54:38 -0500

    Close #45 add refs attribute to elements

--- src/components/appRouter.ts
+++ src/components/appRouter.ts
@@ -11,62 +11,64 @@ const router = useRouter();
 
 router.addRoute("/404", FourOhFour);
 
+router.addRoute("/refs", () => import("src/examples/Refs/Refs"));
+
 router.addRoute(
   "/array-state-persistent-across-route-changes",
-  () => import("src/components/HomePageStatePersistsAcrossRouteChanges")
+  () => import("src/components/HomePageStatePersistsAcrossRouteChanges"),
 );
 router.addRoute(
   "/benchmark",
-  () => import("src/examples/BenchmarkRezact/Benchmark")
+  () => import("src/examples/BenchmarkRezact/Benchmark"),
 );
 router.addRoute("/counter", () => import("src/examples/CounterRezact/Counter"));
 router.addRoute(
   "/counter-multiple",
-  () => import("src/examples/CounterMultipleRezact/CounterMultiple")
+  () => import("src/examples/CounterMultipleRezact/CounterMultiple"),
 );
 router.addRoute("/list", () => import("src/examples/ListRezact/List"));
 router.addRoute(
   "/nested",
-  () => import("src/examples/NestedRezact/NestedChildren")
+  () => import("src/examples/NestedRezact/NestedChildren"),
 );
 router.addRoute(
   "/hello-world",
-  () => import("src/examples/HelloWorldRezact/HelloWorld")
+  () => import("src/examples/HelloWorldRezact/HelloWorld"),
 );
 router.addRoute(
   "/hello-world-multiple",
-  () => import("src/examples/HelloWorldMultipleRezact/HelloWorldMultiple")
+  () => import("src/examples/HelloWorldMultipleRezact/HelloWorldMultiple"),
 );
 router.addRoute("/mdx", () => import("src/components/Test.mdx"));
 
 router.addRoute(
   "/post/:id/something/:test",
-  () => import("src/examples/RouteWithPathParams/index")
+  () => import("src/examples/RouteWithPathParams/index"),
 );
 
 router.addRoute(
   "/shadow-dom",
-  () => import("src/examples/ShadowDom/ShadowDomForExample")
+  () => import("src/examples/ShadowDom/ShadowDomForExample"),
 );
 
 router.addRoute(
   "/data-fetching",
-  () => import("src/examples/DataFetching/DataFetching")
+  () => import("src/examples/DataFetching/DataFetching"),
 );
 
 router.addRoute(
   "/uncontrolled-forms",
-  () => import("src/examples/Forms/UncontrolledForms")
+  () => import("src/examples/Forms/UncontrolledForms"),
 );
 
 router.addRoute(
   "/controlled-forms",
-  () => import("src/examples/Forms/ControlledForms")
+  () => import("src/examples/Forms/ControlledForms"),
 );
 
 router.addRoute(
   "/escape-hatches",
-  () => import("src/examples/EscapeHatches/EscapeHatches")
+  () => import("src/examples/EscapeHatches/EscapeHatches"),
 );
 
 router.addRoute("/todos", () => import("src/examples/Todo/Todo"));
@@ -75,62 +77,62 @@ router.addRoute("/list-redraw", () => import("src/examples/ListReRender/App"));
 
 router.addRoute(
   "/reactive-computations",
-  () => import("src/examples/ReactiveComputations/ReactiveComp")
+  () => import("src/examples/ReactiveComputations/ReactiveComp"),
 );
 
 router.addRoute("/store-test", () => import("src/examples/StoreTest/App"));
 
 router.addRoute(
   "/simple-string-list",
-  () => import("src/examples/SimpleStringListState/SimpleStringList")
+  () => import("src/examples/SimpleStringListState/SimpleStringList"),
 );
 
 router.addRoute(
   "/form-validation",
-  () => import("src/examples/FormValidation/FormValidation")
+  () => import("src/examples/FormValidation/FormValidation"),
 );
 
 router.addRoute(
   "/value-attribute-dot-notation",
-  () => import("src/examples/ValueAttributeDotNotation/ValueAttributeDot")
+  () => import("src/examples/ValueAttributeDotNotation/ValueAttributeDot"),
 );
 
 router.addRoute(
   "/component-context",
-  () => import("src/examples/ComponentContext/ComponentContext")
+  () => import("src/examples/ComponentContext/ComponentContext"),
 );
 
 router.addRoute(
   "/jsx-signals",
-  () => import("src/examples/JsxSignals/JsxSignals")
+  () => import("src/examples/JsxSignals/JsxSignals"),
 );
 
 router.addRoute(
   "/payments/:id",
-  () => import("src/examples/RouteWithPathParams/AmbiguousTest1")
+  () => import("src/examples/RouteWithPathParams/AmbiguousTest1"),
 );
 
 router.addRoute(
   "/payments/ach/:id",
-  () => import("src/examples/RouteWithPathParams/AmbiguousTest2")
+  () => import("src/examples/RouteWithPathParams/AmbiguousTest2"),
 );
 
 router.addRoute(
   "/users",
   () => import("src/examples/NestedRoutes/Users"),
-  nestedRoot
+  nestedRoot,
 );
 router.addRoute(
   "/users/:id",
-  () => import("src/examples/NestedRoutes/UsersId")
+  () => import("src/examples/NestedRoutes/UsersId"),
 );
 router.addRoute(
   "/users/:id/settings",
-  () => import("src/examples/NestedRoutes/UsersIdSettings")
+  () => import("src/examples/NestedRoutes/UsersIdSettings"),
 );
 router.addRoute(
   "/users/:id/settings/*",
-  () => import("src/examples/NestedRoutes/UsersIdSettingsWildCard")
+  () => import("src/examples/NestedRoutes/UsersIdSettingsWildCard"),
 );
 
 const routes = [
