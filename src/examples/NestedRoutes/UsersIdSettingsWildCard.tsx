6cc32a1489026953ce4e807dadbad2a92b18055a
Zach Lankton <zachlankton@gmail.com>
2023-11-19 23:56:11 -0500

    Add error boundary to nested route component

--- src/examples/NestedRoutes/UsersIdSettingsWildCard.tsx
+++ src/examples/NestedRoutes/UsersIdSettingsWildCard.tsx
@@ -1,3 +1,4 @@
+import { ErrorBoundary } from "src/lib/rezact/ErrorBoundary";
 import { MyLayout } from "../Layout/nestedLayout";
 import { Page } from "../SimpleStringListState/SimpleStringList";
 
@@ -11,9 +12,21 @@ export default function ({ router }) {
       <p>ID: {router.params.id}</p>
       <p>{$rest}</p>
       <button onClick={() => ($rest = "wqer/qwer/qwer")}>Test</button>
+      <ErrorBoundary fallback={ErrorComp}>
+        <ErrorTest router={router} />
+      </ErrorBoundary>
       <Page />
     </>
   );
+}
+
+function ErrorTest({ router }) {
+  if (router.pathname.includes("/error-test")) throw new Error("tesT");
+  return <span />;
+}
+
+function ErrorComp() {
+  return <p>Error</p>;
 }
 
 export const Layout = MyLayout;
