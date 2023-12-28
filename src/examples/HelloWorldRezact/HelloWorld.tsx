70edee365a4871ab926e63737a92545b795ef114
Zach Lankton <zachlankton@gmail.com>
2023-12-28 18:26:02 -0500

    actually fix SVG this time, and test

--- src/examples/HelloWorldRezact/HelloWorld.tsx
+++ src/examples/HelloWorldRezact/HelloWorld.tsx
@@ -4,6 +4,20 @@ export function Page() {
   return (
     <>
       <h1>Hello World</h1>
+      <svg
+        class="h-6 w-6 text-white"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke-width="1.5"
+        stroke="currentColor"
+        aria-hidden="true"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M6 18L18 6M6 6l12 12"
+        />
+      </svg>
     </>
   );
 }
