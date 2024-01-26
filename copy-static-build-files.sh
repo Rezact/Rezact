b9dc2a630222318afa88051f4814c8fc240970de
Zach Lankton <zachlankton@gmail.com>
2024-01-26 17:44:44 -0500

    add config and builder plugins

--- copy-static-build-files.sh
+++ copy-static-build-files.sh
@@ -4,4 +4,7 @@ echo "Copying Static Build Files"
 cp package.json dist/
 cp README.md dist/
 cp src/lib/rezact/rezact.d.ts dist/
+cp src/lib/rezact/vite-build-plugin.js dist/
+cp src/lib/rezact/config.js dist/
+cp src/lib/rezact/config.d.ts dist/
 echo "Finished"
